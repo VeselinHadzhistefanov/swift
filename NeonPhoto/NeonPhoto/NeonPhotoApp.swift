32580da877a3cf12907a4d16bc8b796b801cc8ec
veselinhadzhistefanov <v.hadzhistefanov@gmail.com>
2026-08-04 17:56:07 +0300

    downstream branch contents added

---
+++ NeonPhoto/NeonPhoto/NeonPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeonPhotoApp.swift
+//  NeonPhoto
+//
+//  Created by Veselin Hadzhistefanov on 4.08.26.
+//
+
+import SwiftUI
+
+@main
+struct NeonPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
