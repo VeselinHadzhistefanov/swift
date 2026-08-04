27e1a9f73f8f121911f434db62cb333f5a5316e0
veselinhadzhistefanov <v.hadzhistefanov@gmail.com>
2026-08-04 21:24:48 +0300

    updated printing algorithm for layer pattern

--- swift_package_2/Sources/swift_package_2/swift_package_2.swift
+++ swift_package_2/Sources/swift_package_2/swift_package_2.swift
@@ -3,7 +3,26 @@
 
 @main
 struct swift_package_2 {
+    static func printLayersPattern(){
+        let len = 4;
+        var n = 1
+        for i in 0...len-1{
+        let u = (len - i - 1) * 2
+        if(u > 0){
+            for _ in 0...u{
+                print(" ", terminator:"")
+            }
+        }
+        
+        for _ in 0...i{
+            print(String(n) + " ", terminator:"");
+            n += 1
+        }
+        print("")
+        }
+    }
     static func main() {
         print("Hello, world!")
+        printLayersPattern()
     }
 }
