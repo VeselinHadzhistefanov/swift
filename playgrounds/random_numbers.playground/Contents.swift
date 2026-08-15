ce253403acae1c3e93218167c4250c2f969232c3
veselinhadzhistefanov <v.hadzhistefanov@gmail.com>
2026-08-15 19:46:56 +0300

    Re-structure

---
+++ playgrounds/random_numbers.playground/Contents.swift
@@ -0,0 +1,8 @@
+let description = """
+This program will introduce the concept of random numbers in the context of swift, as a programming language and assess it's strengths and weaknesses for casual programmign as well as more cumbersome tasks such as compiling larger data sets of string meterials exploiting the random function as a generating method and further explorations in the direction of effeiciency and optimization.
+"""
+print(description)
+
+let random_int_rage = 
+let random_assigned_variable = Int.random(in: 0...99)
+
