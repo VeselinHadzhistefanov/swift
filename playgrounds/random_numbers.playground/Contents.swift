22cc16b33b02d02c29a99b2d504ab567623cad24
veselinhadzhistefanov <v.hadzhistefanov@gmail.com>
2026-08-17 17:09:10 +0300

    Re-structure

--- playgrounds/random_numbers.playground/Contents.swift
+++ playgrounds/random_numbers.playground/Contents.swift
@@ -1,8 +1,20 @@
-let description = """
+let desc = """
 This program will introduce the concept of random numbers in the context of swift, as a programming language and assess it's strengths and weaknesses for casual programmign as well as more cumbersome tasks such as compiling larger data sets of string meterials exploiting the random function as a generating method and further explorations in the direction of effeiciency and optimization.
 """
-print(description)
+print(desc)
 
-let random_int_rage = 
-let random_assigned_variable = Int.random(in: 0...99)
+func next_random_IS_int(maxValue : Int) -> Int {
+    let range = NSRange
+    let bounds = Range(range: NSMakeRange(location : 0, length: maxValue))
+}
+
+func produce_substring_AS_string(location : Int, length : Int) -> String {
+    
+    
+}
+
+let bounds_IN_int = Range(uncheckedBounds: (lower: 0, upper: 15))
+let int_A_random = Int.random(in : intBounds)
+
+print(desc.)
 
