7f28c81c5e9cf5bb66a7f80cde0be4dded33ca0b
veselinhadzhistefanov <v.hadzhistefanov@gmail.com>
2026-08-24 18:51:48 +0300

    closure expression syntax demonstrations available

--- playgrounds/single_expression_closures.playground/Contents.swift
+++ playgrounds/single_expression_closures.playground/Contents.swift
@@ -1,15 +1,23 @@
 // This playground demonstrates the functionality provided by placing a single expression as the executed code in a single-expression closure. Single-expression closures allow for changing of variables on the fly with the return values automaticaly infered by the context.
 
 struct PersonalObject{
-    let owner: String = "Yolanda"
-    let price: Int = 0
-    let color: String = "Transparent"
-    init(owner : String, price : Int, color : String){
+    var owner: String = "Yolanda"
+    var itemName : String = ""
+    var price: Int = 0
+    var color: String = "Transparent"
+    init(owner : String, itemName : String, price : Int, color : String){
         self.owner = owner
+        self.itemName = itemName
         self.price = price
         self.color = color
     }
 }
 let Simore : String = "Simore"
-let SimoreLoveToy = PersonalObject(owner: Simore, price: 15, color: "Pink")
-func show_members_of_object()
+var SimoreLoveToy = PersonalObject(owner: Simore, itemName: "Deep Love Toy", price: 35, color: "Pink")
+
+func reveal_Object(o : PersonalObject){
+    print("This is " + o.owner + "'s " + o.itemName + " for " + String(o.price) + " in " + o.color)
+}
+
+reveal_Object(o : SimoreLoveToy)
+reveal_Object(o : {SimoreLoveToy.itemName = "Big Pointy Anal Dialation Plug"; return SimoreLoveToy}())
