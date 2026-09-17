4347d50635c3db6fd1812bf3d677a08c317972b7
veselinhadzhistefanov <v.hadzhistefanov@gmail.com>
2026-09-17 20:41:41 +0300

    Requirement set prepared for full algorithm definition

---
+++ playgrounds/ContainerWithMostWater.playground/Contents.swift
@@ -0,0 +1,7 @@
+// Goal: Design a set of functions that allow for finding the pair of indexes of numbers in a array of random numbers that for the biggest area between them from the base of the array (0) to the height of the less tall number in the array.
+
+// Algorithm constrainets (for truth value of requirements)
+// 1. The rectangle is formed between 2 numbers that would form the biggest area.
+// 2. it has to be considered that the array contents can be biased towards higher values in the numbers (create a normalization function for all cases)
+// 3. Choose a specific normalization function to implement.
+// 4. brute form algorithm is allow to test the functions, but a fully fledged function has to be more efficint that a full brdth search
